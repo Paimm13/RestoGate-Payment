7a0712004927b685f8a31ba7ec0b767dfc687379
Paimm13 <fahim.akbar03pagi@gmail.com>
2026-06-21 23:48:26 +0700

    RestoGate Commit

---
+++ src/koneksi/koneksi.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package koneksi;
+
+/**
+ *
+ * @author fahim
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class koneksi {
+    private static Connection conn;
+    public static Connection getConnection() {
+        try {
+            if (conn == null) {
+                String url = "jdbc:mysql://localhost:3306/db_RestoPayment";
+                String user = "root"; // sesuaikan
+                String pass = "";     // sesuaikan
+                conn = DriverManager.getConnection(url, user, pass);
+                System.out.println("Koneksi berhasil!");
+            }
+        } catch (SQLException e) {
+            System.out.println("Koneksi gagal: " + e.getMessage());
+        }
+        return conn;
+    }
+}
+
